73f31ea9c0b79980e6dd41d9e1b49a68ad85298c
DvOmar76 <84674856+DvOmar76@users.noreply.github.com>
2022-01-12 14:01:00 +0300

    Initial Commit

---
+++ GroceryAPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GroceryAPP
+//
+//  Created by DvOmar on 09/06/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
